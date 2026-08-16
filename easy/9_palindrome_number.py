823a704cb49d49ee1282c0f6f42e68a689adfbb5
viskous <151204vishnu@gmail.com>
2026-08-16 19:12:36 +0530

    Add 9. Palindrome Number (Easy)

--- easy/9_palindrome_number.py
+++ easy/9_palindrome_number.py
@@ -1,11 +1,5 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        l = 0
         s = str(x)
-        r = len(s) - 1
-        while l <= r:
-            if s[l] != s[r]:
-                return False
-            l += 1
-            r -= 1
-        return True
+        return s == s[::-1]
+        
