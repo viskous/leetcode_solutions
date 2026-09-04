644244d66e9a6c2d1ab8cf09e891522b282528b1
viskous <151204vishnu@gmail.com>
2026-09-04 12:00:12 +0530

    Add 2022. Convert 1D Array Into 2D Array (Easy)

--- easy/2022_convert_1d_array_into_2d_array.py
+++ easy/2022_convert_1d_array_into_2d_array.py
@@ -1,10 +1,8 @@
 class Solution:
     def construct2DArray(self, original: List[int], m: int, n: int) -> List[List[int]]:
         d2_array = []
-        k = 0
         curr = 0
         nzxt = n
-        print(original[nzxt:nzxt + n])
         if m*n != len(original): return []
         for i in range(m):
             d2_array.append(original[curr:nzxt])
