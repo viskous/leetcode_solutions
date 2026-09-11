49af6a9ef49a7ca010ef886352ace228b2a00efe
viskous <151204vishnu@gmail.com>
2026-09-11 16:33:22 +0530

    Add 2011. Final Value of Variable After Performing Operations (Easy)

---
+++ easy/2011_final_value_of_variable_after_performing_operations.py
@@ -0,0 +1,7 @@
+class Solution:
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+        x = 0
+        for i in operations:
+            if i[1] == "+": x += 1
+            else: x -= 1
+        return x
